f36a8361be12e7d872b41dfb42dbb12ea7e4656d
Amritesh Das <95065530+amriteshdas@users.noreply.github.com>
2025-07-09 07:45:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ def remove_bg():
     return send_file(img_io, mimetype='image/png', as_attachment=True, download_name='output.png')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
